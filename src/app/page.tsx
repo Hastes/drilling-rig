a2c220193b2ad66ba7ccc0f038dd63beff2f80e7
MironovVlad <92030824+MironovVlad@users.noreply.github.com>
2026-07-13 20:18:27 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// src/app/page.tsx
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
@@ -8,6 +9,7 @@ import { TechnicalBase } from "@/components/TechnicalBase";
 import { Documentation } from "@/components/Documentation";
 import { News } from "@/components/News";
 import { Contacts } from "@/components/Contacts";
+import { GeologySection } from "@/components/GeologySection";
 
 export default function Home() {
   return (
@@ -16,6 +18,7 @@ export default function Home() {
       <Hero />
       <About />
       <Activities />
+      <GeologySection />
       <WorkApproach />
       <Geography />
       {/* <Projects /> */}
